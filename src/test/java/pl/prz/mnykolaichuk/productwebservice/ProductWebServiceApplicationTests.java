05762110721e8b3a668984209022ecd4b2d0b45a
Mykola Nykolaichuk <52537767+mnykolaichuk96@users.noreply.github.com>
2023-03-08 23:05:47 +0100

    ProductController create, getAll + mongodb

--- src/test/java/pl/prz/mnykolaichuk/productwebservice/ProductWebServiceApplicationTests.java
+++ src/test/java/pl/prz/mnykolaichuk/productwebservice/ProductWebServiceApplicationTests.java
@@ -2,12 +2,31 @@ package pl.prz.mnykolaichuk.productwebservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+//junit5 zrozumi ze bedzie wykorzystany testcontainer do testow
+@Testcontainers
 class ProductWebServiceApplicationTests {
+    // junit understand that  this is container
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.4");
 
+    @DynamicPropertySource      // doda propierties dynamicznie do kontenera
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+    /*
+    * Test uruchomi mongodb kontainer sciagajac image: mongodb:6.0.4
+    * Pobierze replikaset url i doda ten url do spring.data.mongodb.uri
+    * */
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() {
+
     }
 
 }
